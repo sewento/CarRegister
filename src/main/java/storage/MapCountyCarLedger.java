767bde76c7dc1a8aa259f3823d717ffdd615ed11
DS <dawid.sekula@gmail.com>
2019-09-07 12:10:32 +0200

    added ListCountyCarLedger; added MapCountyCarLedger;

--- src/main/java/storage/MapCountyCarLedger.java
+++ src/main/java/storage/MapCountyCarLedger.java
@@ -1,4 +1,45 @@
 package storage;
 
-public class MapCountyCarLedger {
+import model.Car;
+import model.County;
+
+import java.util.*;
+
+public class MapCountyCarLedger implements CountyCarLedger {
+
+    private Map<County, Set<Car>> countyCarMap;
+
+    public MapCountyCarLedger(){
+        countyCarMap = new HashMap<>();
+    }
+
+    @Override
+    public void addCar(Car car) {
+        County county = car.getCounty();
+        if (countyCarMap.containsKey(county)){
+            countyCarMap.get(county).add(car);
+        }else{
+            countyCarMap.put(county, new TreeSet<>());
+            countyCarMap.get(county).add(car);
+        }
+    }
+
+    @Override
+    public void removeCar(Car car) {
+        County county = car.getCounty();
+
+        if (countyCarMap.containsKey(county)){
+            countyCarMap.get(county).remove(car);
+        }
+
+    }
+
+    @Override
+    public List<Car> findAllCars() {
+        List<Car> carList = new ArrayList<>();
+        for (County county : countyCarMap.keySet()){
+            carList.addAll(countyCarMap.get(county));
+        }
+        return carList;
+    }
 }
