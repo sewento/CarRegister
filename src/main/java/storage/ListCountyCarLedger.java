b05f5a2493373a2458443f203a6aebba0f316894
kamilwieczorek90 <kamilwieczorek90@gmail.com>
2019-09-07 11:18:05 +0200

    created add method test

--- src/main/java/storage/ListCountyCarLedger.java
+++ src/main/java/storage/ListCountyCarLedger.java
@@ -1,4 +1,22 @@
 package storage;
 
-public class ListCountyCarLedger {
+import model.Car;
+
+import java.util.List;
+
+public class ListCountyCarLedger implements CountyCarLedger {
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void removeCar(Car car) {
+
+    }
+
+    @Override
+    public List<Car> findAllCars() {
+        return null;
+    }
 }
--- src/main/java/storage/MapCountyCarLedger.java
+++ src/main/java/storage/MapCountyCarLedger.java
@@ -1,4 +1,22 @@
 package storage;
 
-public class MapCountyCarLedger {
+import model.Car;
+
+import java.util.List;
+
+public class MapCountyCarLedger implements CountyCarLedger{
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void removeCar(Car car) {
+
+    }
+
+    @Override
+    public List<Car> findAllCars() {
+        return null;
+    }
 }
