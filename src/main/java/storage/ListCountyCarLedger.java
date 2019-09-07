b615beaa9bcbd0c00d208a6c0f2682e5b752b447
Michal Wieczorek <wieczorekmichal1991@gmail.com>
2019-09-07 11:15:04 +0200

    Implements methods in ListCountyCarLedger

--- src/main/java/storage/ListCountyCarLedger.java
+++ src/main/java/storage/ListCountyCarLedger.java
@@ -5,8 +5,25 @@ import model.Car;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListCountyCarLedger {
+public class ListCountyCarLedger implements CountyCarLedger {
 
-    List<Car>  cars = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+
+    @Override
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    @Override
+    public void removeCar(Car car) {
+        cars.remove(car);
+
+    }
+
+    @Override
+    public List<Car> findAllCars() {
+        return cars;
+    }
+
 
 }
