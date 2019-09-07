f4e6ddb9d1c8e4d89de1ba1782409072b9445d19
Michal Wieczorek <wieczorekmichal1991@gmail.com>
2019-09-07 12:04:17 +0200

    Implements methods FindAllByCounty in ListCountyCarLedger

--- src/main/java/storage/ListCountyCarLedger.java
+++ src/main/java/storage/ListCountyCarLedger.java
@@ -1,9 +1,14 @@
 package storage;
 
 import model.Car;
+import model.County;
+
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListCountyCarLedger implements CountyCarLedger {
 
@@ -17,13 +22,14 @@ public class ListCountyCarLedger implements CountyCarLedger {
     @Override
     public void removeCar(Car car) {
         cars.remove(car);
-
     }
 
     @Override
     public List<Car> findAllCars() {
+        List<Car> car = cars
+                .stream()
+                .sorted(Comparator.comparing(Car::getCounty))
+                .collect(Collectors.toList());
         return cars;
     }
-
-
 }
--- src/test/java/storage/CountyCarLedgerTest.java
+++ src/test/java/storage/CountyCarLedgerTest.java
@@ -1,7 +1,34 @@
 package storage;
 
+import model.Car;
+import model.NormalCar;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountyCarLedgerTest {
 
+    @Test
+    void addCarToMap_givenOneCar_shouldHaveOneObjectInMap (){
+        Car car = new NormalCar();
+        CountyCarLedger ledger = new MapCountyCarLedger();
+        addCheckTest(ledger, car);
+    }
+    @Test
+    void addCarToList_givenOneCar_shouldHaveOneObjectInList (){
+        Car car = new NormalCar();
+        CountyCarLedger ledger = new ListCountyCarLedger();
+        addCheckTest(ledger, car);
+
+    }
+
+    private void addCheckTest(CountyCarLedger countyCarLedger, Car car){
+        countyCarLedger.addCar(car);
+        assertEquals(1, countyCarLedger.findAllCars().size());
+    }
 }
+
+
+//    void    addCar(Car car);
+//    void removeCar(Car car);
+//    List<Car> findAllCars();
